9ad36c7d512e09ea6a1ac6d9182e33f4d4bee56d
382333604@qq.com <xxx@xxx.com>
2020-11-09 23:09:39 +0800

    增加：自己实现 IOC 第一步

---
+++ ssm-salt-ioc/src/main/java/cn/edu/cqvie/ioc/config/AppConfig.java
@@ -0,0 +1,14 @@
+package cn.edu.cqvie.ioc.config;
+
+import cn.edu.cqvie.ioc.annotation.ComponentScan;
+
+/**
+ * ioc 扫描范围定义
+ *
+ * @author zhengsh
+ * @date 2020-11-091
+ */
+@ComponentScan("cn.edu.cqvie.ioc.service")
+public class AppConfig {
+
+}
---
+++ ssm-salt-ioc/src/main/java/cn/edu/cqvie/ioc/service/OrderService.java
@@ -0,0 +1,8 @@
+package cn.edu.cqvie.ioc.service;
+
+import cn.edu.cqvie.ioc.annotation.Component;
+
+@Component
+public class OrderService {
+    
+}
