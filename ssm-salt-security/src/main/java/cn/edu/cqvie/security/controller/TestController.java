5611e259189f376a728bf00840313aa58854cf1d
zhengshaohong <zhengshaohong123>
2020-11-10 15:39:17 +0800

    修改：控制器添加单元测试

---
+++ ssm-salt-security/src/main/java/cn/edu/cqvie/security/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.edu.cqvie.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return  "Hello security";
+    }
+}
