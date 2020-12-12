ebe5079df0cb99eb6ba538a39fe9d69036fa05eb
zhengshaohong <zhengshaohong123>
2020-12-12 15:48:19 +0800

    增加：jvm 字节码查看插件

---
+++ ssm-salt-jvm/src/main/java/cn/edu/cqvie/jvm/Test1.java
@@ -0,0 +1,12 @@
+package cn.edu.cqvie.jvm;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Object[] arr2 = new Object[5];
+
+        System.out.println(arr);
+        System.out.println(arr2);
+    }
+}
