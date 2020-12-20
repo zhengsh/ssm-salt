820ede1f744d4fc2838e12011f485c1565feaea4
382333604@qq.com <xxx@xxx.com>
2020-12-20 11:40:28 +0800

    本地备份

--- ssm-salt-jvm/src/main/java/cn/edu/cqvie/jvm/Test1.java
+++ ssm-salt-jvm/src/main/java/cn/edu/cqvie/jvm/Test1.java
@@ -2,11 +2,16 @@ package cn.edu.cqvie.jvm;
 
 public class Test1 {
 
+    private static String strA = "strA";
+
+
     public static void main(String[] args) {
         int[] arr = new int[5];
         Object[] arr2 = new Object[5];
 
+        System.out.println(strA);
         System.out.println(arr);
+
         System.out.println(arr2);
     }
 }
--- ssm-salt-webflux/src/main/java/cn/edu/cqvie/webflux/Application.java
+++ ssm-salt-webflux/src/main/java/cn/edu/cqvie/webflux/Application.java
@@ -14,7 +14,12 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @EnableR2dbcRepositories
 public class Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        for (int i = 2020; i <= 2020; i++) { //Ara You Ok!
+            System.out.println("we work in 996, love life"); // 2020 总结：代码太多，故事太少
+        } //2020 Say Goodbye
     }
 }
