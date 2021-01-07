8622f0d0eb79b5e55429f824af1d5be0b816104b
382333604@qq.com <xxx@xxx.com>
2021-01-07 21:15:43 +0800

    观察者模式和消息机制

--- ssm-salt-jdk/src/main/java/cn/edu/cqvie/dp/ObserverPatternDemo.java
+++ ssm-salt-jdk/src/main/java/cn/edu/cqvie/dp/ObserverPatternDemo.java
@@ -10,25 +10,19 @@ public class ObserverPatternDemo {
   static Subject subject = new Subject();
 
    public static void main(String[] args) {
+
+      Integer careNumber = 10; //订单号
+
       // 等待线程
       Lock lock = new Lock();
       ThreadA a = new ThreadA(lock);
       a.start();
 
-      Integer careNumber = 10;
-      // 观察者
-      new HexaObserver(subject, 0);
-      // 观察者
-      new OctalObserver(subject, 0);
-      // 观察者
-      new BinaryObserver(subject, 0);
       // 观察者
       new ThreadObserver(subject, careNumber, lock);
 
       // 1. 通知观察者
-      System.out.println("First state change: 15");
-      subject.setState(15);
-      System.out.println("Second state change: 10");  
-      subject.setState(10);
+      System.out.println("Second state change: careNumber");
+      subject.setState(careNumber);
    }
 }
--- ssm-salt-jdk/src/main/java/cn/edu/cqvie/dp/observer/ThreadObserver.java
+++ ssm-salt-jdk/src/main/java/cn/edu/cqvie/dp/observer/ThreadObserver.java
@@ -2,28 +2,31 @@ package cn.edu.cqvie.dp.observer;
 
 import cn.edu.cqvie.dp.notify.Lock;
 
-public class ThreadObserver extends Observer{
+public class ThreadObserver extends Observer {
 
-   protected Lock object;
+    protected Lock object;
 
-   public ThreadObserver(Subject subject, Integer careNumber, Lock object){
-      this.subject = subject;
-      this.careNumber = careNumber;
-      this.object = object;
-      this.subject.attach(this);
-   }
- 
-   @Override
-   public void update() {
-      System.out.println( "Thread String: " + Integer.toBinaryString( subject.getState() ) );
-      try {
-         Thread.sleep(20000L);
-      } catch (InterruptedException e) {
-         e.printStackTrace();
-      }
-      synchronized (this.object) {
-         this.object.lockCode = String.valueOf(System.currentTimeMillis());
-         this.object.notifyAll();
-      }
-   }
+    public ThreadObserver(Subject subject, Integer careNumber, Lock object) {
+        this.subject = subject;
+        this.careNumber = careNumber;
+        this.object = object;
+        this.subject.attach(this);
+    }
+
+    /**
+     * 线程观察者
+     */
+    @Override
+    public void update() {
+        System.out.println("Thread String: " + Integer.toBinaryString(subject.getState()));
+        try {
+            Thread.sleep(20000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized (this.object) {
+            this.object.lockCode = String.valueOf(System.currentTimeMillis());
+            this.object.notifyAll();
+        }
+    }
 }
